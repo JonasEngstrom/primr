e76ff839b2de46f08bbde251ac92702bf3600636
Jonas Engström <56550304+JonasEngstrom@users.noreply.github.com>
2022-03-23 14:49:54 +0100

    generate_primes() passes all tests.

--- R/generate_primes.R
+++ R/generate_primes.R
@@ -14,9 +14,28 @@
 #'
 #' @seealso [primr::make_encoding_key()]
 #'
+#' @import assertthat
+#'
 #' @examples
 #' # Return a vector containing 2, 3, and 5.
 #' vector_of_primes <- generate_primes(3)
 generate_primes <- function(number_of_primes) {
+
+  # Check that argument number_of_prime is a positive integer,
+  # even if it might be stored in a double.
+  assertthat::assert_that((number_of_primes %% 1 == 0 & number_of_primes >= 1),
+                          msg = "Argument number_of_primes needs to be a positive integer.")
+
+  vector_of_primes <- 2
+  counter <- 3
+
+  # Loop through and store numbers that are not divisible by primes, i.e. other primes.
+  while (length(vector_of_primes) < number_of_primes) {
+    if (sum(counter %% vector_of_primes < 1) == 0) {
+      vector_of_primes <- c(vector_of_primes, counter)
+    }
+    counter <- counter + 2
+  }
+
   return(vector_of_primes)
 }
