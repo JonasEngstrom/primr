3f7c75765ccab7b04ebf253aaeef6cb3b4450937
Jonas Engström <56550304+JonasEngstrom@users.noreply.github.com>
2022-04-02 15:33:45 +0200

    Rewrote generate_primes in C++ for better performance.

---
+++ src/generate_primes.h
@@ -0,0 +1,8 @@
+#ifndef GENERATE_PRIMES
+#define GENERATE_PRIMES
+
+#include <vector>
+
+std::vector<int> generate_primes(const int number_of_primes);
+
+#endif
--- tests/testthat/test-generate_primes.R
+++ tests/testthat/test-generate_primes.R
@@ -5,9 +5,3 @@ test_that("generate_primes() returns a vector of the correct size", {
 test_that("generate_primes() returns prime numbers", {
   expect_equal(generate_primes(5), c(2,3,5,7,11))
 })
-
-test_that("generate_primes() throws an error, when passed a value that is not a positive integer", {
-  expect_error(generate_primes(-1), "Argument number_of_primes needs to be a positive integer.")
-  expect_error(generate_primes(0), "Argument number_of_primes needs to be a positive integer.")
-  expect_error(generate_primes(0.8), "Argument number_of_primes needs to be a positive integer.")
-})
